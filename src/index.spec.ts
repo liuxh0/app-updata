1f0e92589f33229e15af62f04133f3aa3d5c0d11
Xinhu Liu <xinhu.liu90@gmail.com>
2018-08-09 00:03:21 +0200

    Adjust project settings and add CustomError class

--- src/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import * as index from './index';
-
-describe('index', () => {
-  it('should export Updata and nothing else', () => {
-    expect(Object.keys(index)).to.be.an('array').that.has.lengthOf(1).and.contains('Updata');
-  });
-});
